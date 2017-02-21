6dbf8786168cffddb5c5676f790f407d795c02e0
Bill Waggoner <ctgreybeard@gmail.com>
2017-02-21 11:48:16 -0500

    Initial XCode Project setup

---
+++ Swift Custom Control/Swift Custom Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift Custom Control
+//
+//  Created by William Waggoner on 2/21/17.
+//  Copyright © 2017 William C Waggoner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift Custom Control/Swift Custom Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift Custom Control
+//
+//  Created by William Waggoner on 2/21/17.
+//  Copyright © 2017 William C Waggoner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
